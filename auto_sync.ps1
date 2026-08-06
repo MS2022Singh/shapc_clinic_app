2ee214d1a825406f2e62e21338bb7523c0e25297
MS2022Singh <meenakshisingh7@gmail.com>
2026-08-06 11:47:11 +0530

    Aapka update message yahan likhein

--- auto_sync.ps1
+++ auto_sync.ps1
@@ -1,5 +1,5 @@
-﻿param([string] = "Auto update clinic app features")
+﻿param([string]$commitMessage = "Auto update clinic app features")
 git add .
-git commit -m ""
+git commit -m "$commitMessage"
 git push origin main
 Write-Host "Changes successfully pushed to GitHub!" -ForegroundColor Green
