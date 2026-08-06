d0f1bfe2078d6301b1af940c16c3185e31e00337
MS2022Singh <meenakshisingh7@gmail.com>
2026-08-06 11:43:51 +0530

    Initial commit - setup clinic app repo

---
+++ android/app/src/main/kotlin/com/example/shapc_clinic_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shapc_clinic_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ auto_sync.ps1
@@ -0,0 +1,5 @@
+﻿param([string] = "Auto update clinic app features")
+git add .
+git commit -m ""
+git push origin main
+Write-Host "Changes successfully pushed to GitHub!" -ForegroundColor Green
